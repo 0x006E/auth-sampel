ac8795a1dd9b2de6379d099ed4bcaa9bf41f9f32
Nithin S Varrier <nithin486@hotmail.com>
2022-07-22 22:36:13 +0530

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <SnackbarProvider>
       <Layout>
-        <span className="w-80 h-80 none"></span>
+        <span className="w-20 h-20 none"></span>
         <div className="bg-white p-8 flex flex-col w-full shadow-xl rounded-xl">
           <BrowserRouter>
             <Routes>
